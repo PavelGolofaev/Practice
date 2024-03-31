1195023a9c7a01201fe2dced48d49f12bd6e67be
Pavel Golofaev~ <pavel.golofaev@gmail.com~>
2024-03-31 17:09:30 +0300

    add lesson2 and lesson3

---
+++ Lesson2/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// // Напишите программу, которая принимает на вход трёхзначное число и удаляет вторую цифру этого числа.
+// Примеры
+// a = 256=> 26
+// a = 891=> 81
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num <= 999 || num <= -100 && num >= -999)
+{
+    int digit1 = num / 100;
+    int digit3 = num % 10;
+    int result = digit1 * 10 + digit3;
+    Console.Write(result);
+}
+else
+{
+    Console.WriteLine("некорректный ввод");
+}
---
+++ Lesson3/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿//int[] array = new int[6]{1, 3, 4, 19, 3, 8};
+
+// 1. Получаем/задаём исходные данные
+int[] array = {1, 3, 4, 19, 3, 8};
+int num = 9;
+
+// int[] arr = new int[3];
+// for (int i = 0; i < arr.Length; i++)
+// {
+// Console.Write($"Введите {i + 1} элемент: ");
+// arr[i] = Convert.ToInt32(Console.ReadLine());
+// }
+
+// 2. Проверка исходных данных (при необходимости)
+
+// 3. Реализация логики (алгоритм)
+bool res = false;
+
+for (int i = 0; i < array.Length; i++)
+{
+if(array[i] == num)
+{
+res = true;
+break;
+}
+}
+
+// 4. Вывод результата
+Console.WriteLine(res ? "Да" : "Нет");
