ca7b2884a30c81fbc305aa7b23dfa08050789609
Pavel Golofaev~ <pavel.golofaev@gmail.com~>
2024-03-31 18:42:57 +0300

    update Task1

--- Lesson4/Task1/Program.cs
+++ Lesson4/Task1/Program.cs
@@ -10,24 +10,66 @@
 
 int[] CreateArrayRndInt(int size, int min, int max)
 {
-    int[] array = new int[size];
-    Random rnd = new Random();
+int[] array = new int[size];
+Random rnd = new Random();
 
-    for (int i = 0; i < size; i++)
-    {
-        array[i] = rnd.Next(min, max);
-    }
+for (int i = 0; i < size; i++)
+{
+array[i] = rnd.Next(min, max);
+}
 
-    return array;
+return array;
 }
 
 void PrintArray(int[] array)
 {
-     for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+if (i < array.Length - 1)
+{
+Console.Write($"{array[i]}, ");
+}
+else
+{
+Console.Write($"{array[i]}");
+}
+
+}
+Console.Write("]");
+}
+
+int CountPrime(int[] array)
+{
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+if (IsPrime(array[i]))
+{
+count++;
+}
+}
+
+return count;
+}
+
+bool IsPrime(int num)
+{
+for (int i = 2; i <= Math.Sqrt(num); i++)
+{
+if (num % i == 0)
+{
+return false;
+}
+}
+
+return true;
 }
 
 int[] arr = CreateArrayRndInt(10, 1, 100);
-PrintArray(arr); 
+PrintArray(arr);
+
+//Console.WriteLine();
+int countPrime = CountPrime(arr);
+Console.WriteLine($" => {countPrime}");
